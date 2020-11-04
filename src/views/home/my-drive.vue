793eb6bdf00a50bb3c5f3a0208f9721761e6f13d
始终 <525140052@qq.com>
2020-11-05 00:04:00 +0800

    ali sts

--- src/views/home/my-drive.vue
+++ src/views/home/my-drive.vue
@@ -24,26 +24,36 @@
 </template>
 
 <script>
-import {UserRegist} from "@/api/product";
+// import {UserRegist} from "@/api/product";
 // import util from '@/libs/util.js'
-
+// import axios from 'axios';
+import {aliSTS2} from "@/api/alists";
 export default {
   data() {
     return {}
   },
-  filters: {},
+  filters: {
+
+  },
   methods: {
 
     testClick() {
-      console.log(1231354565)
+      // const url = 'https://sts.aliyuncs.com';
+      // console.log(url)
+      aliSTS2()
 
-      const formData = {
-        nickName: "nickName",
-      };
-      // 调接口请求
-      UserRegist(formData).then(res => {
-        console.log(res)
-      })
+      // axios.get(url).then(
+      //     response => {
+      //       const result = response.data.items[0];
+      //       console.log(result)
+      //     }
+      // ).catch(
+      //     response => {
+      //       alert('请求失败');
+      //       console.log(response)
+      //     },
+      // );
+
     },
   }
 }
