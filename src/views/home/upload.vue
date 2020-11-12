07404597df610b73a7ade5eabd611c81c10f1789
linjoe <525140052@qq.com>
2020-11-12 18:46:00 +0800

    上传文件

--- src/views/home/upload.vue
+++ src/views/home/upload.vue
@@ -33,7 +33,7 @@
 
       <v-card>
         <v-card-title class="headline grey darken-4 lighten-2">
-          {{card_title}}
+          {{ card_title }}
           <v-spacer></v-spacer>
 
           <v-btn
@@ -46,7 +46,11 @@
         </v-card-title>
 
         <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
         </v-card-text>
 
         <v-divider></v-divider>
@@ -63,72 +67,44 @@ export default {
   data() {
     return {
       name: "upload",
-      card_title:"正在上传",
-      upload_file_dialog : false,
-      file: {
-        name: '',
-        url: '',
-        extension: '',
-        file: ''
-      },
+      card_title: "正在上传",
+      upload_file_dialog: false,
       uploadState: false,
-      files: [],
-      error: {
-        state: false,
-        msg: ''
-      },
+      file_list: [[]],
       responseType: 'success',
       fileType: 'image/*',
       fileSize: '524288'
     }
 
   },
-  components: {
-  },
-  methods:{
-    pickFile () {
-      alert('pickFile')
-      this.alert()
-      this.$refs.fileInput.click()
-    },
-    onFilePicked(e){
-      console.log(e)
+  components: {},
+  methods: {
 
-      const selectedFiles = e
-      if (selectedFiles[0]) {
+    onFilePicked(files_data) {
 
-        if (selectedFiles[0].size >= this.fileSize) {
-          this.alert(true, `File is too big. ${selectedFiles[0].size} Bytes`)
+      files_data.forEach((item, key) => {
+
+        if (item.size >= this.fileSize) {
+          this.alert(true, `File is too big. ${item.size} Bytes`)
         } else {
-          this.file.name = selectedFiles[0].name
-          this.file.extension = this.file.name.substr(this.file.name.indexOf('.') + 1)
-          if (this.file.name.lastIndexOf('.') <= 0) {
-            return
-          }
+          this.file_list[key] = [];
+          this.file_list[key]['name'] = item['name']
+          this.file_list[key].extension = this.file_list[key].name.substr(this.file_list[key].name.indexOf('.') + 1)
+          // if (this.file_list[key].name.lastIndexOf('.') <= 0) {
+          //   this.$delete(this.file_list,key)
+            // break;
+          // }
           const fr = new FileReader()
-          fr.readAsDataURL(selectedFiles[0])
+          fr.readAsDataURL(item)
           fr.addEventListener('load', () => {
-            this.file.url = fr.result
-            this.file.file = selectedFiles[0]
+            this.file_list[key].url = fr.result
+            this.file_list[key].file = item
           })
+
         }
-        console.log(this.file)
-
-      } else {
-        this.defaults()
-      }
+      })
     },
 
-    defaults () {
-      this.file.name = ''
-      this.file.file = ''
-      this.file.url = ''
-      this.file.extension = ''
-    },
-    alert (boolean, msg) {
-      this.error.state = boolean || false
-      this.error.msg = msg || ''
-    }
   }
 }
 </script>
