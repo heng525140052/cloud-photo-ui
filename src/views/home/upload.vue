5634af0131aec2b2f4f61119c1e00d409e1250e9
始终 <525140052@qq.com>
2020-11-12 23:17:27 +0800

    上传图片列表初始版本

--- src/views/home/upload.vue
+++ src/views/home/upload.vue
@@ -15,6 +15,73 @@
     ></v-file-input>
     <v-spacer></v-spacer>
 
+
+    <v-card
+        class="mx-auto"
+        max-width="500"
+    >
+      <v-toolbar
+          color="pink"
+          dark
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Inbox</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-checkbox-marked-circle</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+      <v-list two-line>
+        <v-list-item-group
+            v-model="selectedFile"
+            active-class="pink--text"
+            multiple
+        >
+          <template v-for="(item, index) in upload_file_list">
+            <v-list-item :key="item.title">
+              <template v-slot:default="{ active }">
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.name"></v-list-item-title>
+                  <img :src="item.url" alt="">
+                </v-list-item-content>
+
+                <v-list-item-action>
+                  <v-list-item-action-text v-text="item.extension"></v-list-item-action-text>
+                  <v-icon
+                      v-if="!active"
+                      color="grey lighten-1"
+                  >
+                    mdi-star-outline
+                  </v-icon>
+
+                  <v-icon
+                      v-else
+                      color="yellow darken-3"
+                  >
+                    mdi-star
+                  </v-icon>
+                </v-list-item-action>
+              </template>
+            </v-list-item>
+
+            <v-divider
+                v-if="index < upload_file_list.length - 1"
+                :key="index"
+            ></v-divider>
+          </template>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+
+
     <v-dialog
         v-model="upload_file_dialog"
         hide-overlay
@@ -57,6 +124,8 @@
 
       </v-card>
     </v-dialog>
+
+
   </div>
 
 </template>
@@ -70,10 +139,19 @@ export default {
       card_title: "正在上传",
       upload_file_dialog: false,
       uploadState: false,
-      file_list: [[]],
+      upload_file_list: [
+        {
+          name: '测试.jpg',
+          extension: 'jpg',
+          url: 'aaa/aaa/jpg',
+          file: ''
+        }
+      ],
       responseType: 'success',
       fileType: 'image/*',
-      fileSize: '524288'
+      fileSize: '524288',
+
+      selectedFile: []
     }
 
   },
@@ -87,22 +165,27 @@ export default {
         if (item.size >= this.fileSize) {
           this.alert(true, `File is too big. ${item.size} Bytes`)
         } else {
-          this.file_list[key] = [];
-          this.file_list[key]['name'] = item['name']
-          this.file_list[key].extension = this.file_list[key].name.substr(this.file_list[key].name.indexOf('.') + 1)
-          // if (this.file_list[key].name.lastIndexOf('.') <= 0) {
-          //   this.$delete(this.file_list,key)
-            // break;
+          this.upload_file_list[key] = [];
+          this.upload_file_list[key]['name'] = item['name']
+          this.upload_file_list[key].extension = this.upload_file_list[key].name.substr(this.upload_file_list[key].name.indexOf('.') + 1)
+          // if (this.upload_file_list[key].name.lastIndexOf('.') <= 0) {
+          //   this.$delete(this.upload_file_list,key)
+          // break;
           // }
           const fr = new FileReader()
           fr.readAsDataURL(item)
           fr.addEventListener('load', () => {
-            this.file_list[key].url = fr.result
-            this.file_list[key].file = item
+            this.upload_file_list[key].url = fr.result
+            this.upload_file_list[key].file = item
+
+            console.log(this.upload_file_list, 'upload success', key)
           })
 
         }
       })
+      console.log("this.upload_file_list")
+      console.log(this.upload_file_list)
+      console.log("this.upload_file_list")
     },
 
   }
