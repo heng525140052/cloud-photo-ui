c7e7daf1c6611d248c323d8961d0bc0d2fb2f27d
linjoe <525140052@qq.com>
2020-11-19 17:27:35 +0800

    点击隐藏上传列表

--- src/views/home/upload.vue
+++ src/views/home/upload.vue
@@ -11,57 +11,56 @@
         accept="image/*"
         label="File input"
         truncate-length="15"
-        @change="onFilePicked"
-    ></v-file-input>
+        @change="UploadFile"
+    >真的吗
+    </v-file-input>
     <v-spacer></v-spacer>
 
 
     <v-card
-        class="mx-auto"
+        class="mx-auto upload-file-list"
         max-width="500"
+
     >
       <v-toolbar
           color="pink"
           dark
       >
 
-        <v-toolbar-title>Inbox</v-toolbar-title>
+        <v-toolbar-title>上传文件列表</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn icon>
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-checkbox-marked-circle</v-icon>
+        <v-btn icon @click="uploadFileListState = !uploadFileListState">
+          <v-icon>mdi-minus</v-icon>
         </v-btn>
       </v-toolbar>
 
-      <v-list two-line>
+      <v-list>
         <v-list-item-group
-            v-model="selectedFile"
             active-class="pink--text"
             multiple
+            :hidden="uploadFileListState"
         >
-          <template v-for="(item, index) in upload_file_list">
-            <v-list-item :key="item.title">
+          <template v-for="(item, index) in upload_file_list" >
+            <v-list-item :key="item.title" >
               <template>
-
-                <v-list-item-action>
+                <v-list-item-avatar>
                   <v-icon large color="green darken-2">{{ item.file_icon }}</v-icon>
-                </v-list-item-action>
-
+                </v-list-item-avatar>
                 <v-list-item-content>
                   <v-list-item-title v-text="item.name"></v-list-item-title>
                 </v-list-item-content>
-
                 <v-list-item-action>
                   <v-list-item-action>
-                    <v-icon large color="green darken-2">{{ item.file_icon }}</v-icon>
+                    <v-btn class="ma-2" color="primary" dark @v-on="changeUploadFileButton">
+                      <v-icon dark right>mdi-checkbox-marked-circle</v-icon>
+                    </v-btn>
                   </v-list-item-action>
-                  <v-list-item-action-text v-text="item.extension"></v-list-item-action-text>
                 </v-list-item-action>
               </template>
             </v-list-item>
-
             <v-divider
                 v-if="index < upload_file_list.length - 1"
                 :key="index"
@@ -70,14 +69,6 @@
         </v-list-item-group>
       </v-list>
     </v-card>
-
-    <v-btn
-        depressed
-        color="primary"
-        @click="uploadFile"
-    >
-      Primary
-    </v-btn>
 
 
   </div>
@@ -100,14 +91,15 @@ export default {
       responseType: 'success',
       fileType: 'image/*',
       fileSize: '5242880',
-      selectedFile: []
+      uploadFileListState: false,
+      uploadFileListFileState: false,
     }
 
   },
   components: {},
   methods: {
 
-    onFilePicked(files_data) {
+    UploadFile(files_data) {
 
       files_data.forEach((item) => {
 
@@ -121,8 +113,6 @@ export default {
             let item_extension = item_name.substr(item_name.indexOf('.') + 1)
             let file_icon = '';
 
-
-
             let filetype = util.matchType(item.type)
 
             if (filetype === 'image') {
@@ -131,42 +121,49 @@ export default {
               file_icon = 'mdi-file-video'
             } else if (filetype === 'audio') {
               file_icon = 'mdi-book-music'
-            }else if (filetype === 'other') {
+            } else if (filetype === 'other') {
               file_icon = 'mdi-file'
             }
-            console.log('file_icon')
-            console.log(filetype)
-            console.log(file_icon)
-            console.log('file_icon')
 
-            this.upload_file_list.push({
+            let file_item = {
               name: item_name,
               extension: item_extension,
               upload_status: false,
               url: fr.result,
               file_icon: file_icon,
               file: item
-            })
+            }
+            this.upload_file_list.push(file_item)
+
+            AliOssController.put(file_item).then(data => {
+              console.log(data);
+            });
 
           })
         }
       })
-      console.log(this.upload_file_list)
     },
-    uploadFile() {
-      const params = this.upload_file_list[0];
 
-      console.log(params)
+    changeUploadFileButton() {
+      console.log('++++++++++++++++++')
+      console.log(this.uploadFileListState)
 
-      AliOssController.put(params).then(data => {
-        console.log(data);
-      });
-    }
+      this.uploadFileListState = true
+      console.log('-----------------')
+
+      console.log(this.uploadFileListState)
+      console.log('++++++++++++++++++')
+    },
 
   }
 }
 </script>
 
 <style scoped>
+
+.upload-file-list {
+  /*padding-left: 10px;*/
+}
+
 
 </style>
