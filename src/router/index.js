186ece58992a30bd4472e09a4af570bb977b480d
始终 <525140052@qq.com>
2020-11-23 21:58:38 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import { publicRoute, protectedRoute } from './config'
-const routes = protectedRoute
+// import { publicRoute, protectedRoute } from './config'
+// const routes = protectedRoute
+import LayoutDefault from '@/components/layouts/LayoutDefault.vue'
+
 
 Vue.use(Router)
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        components: {
+            LayoutDefault,
+            default: () => import(/* webpackChunkName: "about" */ '../views/home/folders')
+        }
+    },
+]
+
 const router = new Router({
     mode: 'hash',
     linkActiveClass: 'active',
